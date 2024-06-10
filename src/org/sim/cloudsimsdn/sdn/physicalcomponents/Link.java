4f0460fb2ce85551223dc2532753ad2e3bb678d5
JaYung136 <1710349420@qq.com>
2024-06-10 21:34:17 +0800

    .

--- src/org/sim/cloudsimsdn/sdn/physicalcomponents/Link.java
+++ src/org/sim/cloudsimsdn/sdn/physicalcomponents/Link.java
@@ -15,6 +15,7 @@ import org.sim.cloudsimsdn.sdn.physicalcomponents.switches.GatewaySwitch;
 import org.sim.cloudsimsdn.sdn.physicalcomponents.switches.IntercloudSwitch;
 import org.sim.cloudsimsdn.sdn.virtualcomponents.Channel;
 import org.sim.controller.LinkUtil;
+import org.sim.service.Constants;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -253,7 +254,7 @@ public class Link {
 				&& this.highOrder instanceof IntercloudSwitch != true
 				&& this.lowOrder instanceof GatewaySwitch != true
 				&& this.highOrder instanceof GatewaySwitch != true){
-			LogWriter log = LogWriter.getLogger("./OutputFiles/bandwidthUtil/link_utilization.xml");
+			LogWriter log = LogWriter.getLogger(Constants.outputPath+"\\link_utilization.xml");
 			log.printLine("\t<Link Name=\"" +this.linkname+ "\" Starttime=\"" +String.format("%.6f", logTime-timeUnit) + "\" Endtime=\"" +String.format("%.6f", logTime) +"\" Src=\"" +this.lowOrder+ "\" Dst=\"" +this.highOrder+ "\" KBytes=\"" +monitoringProcessedBytesPerUnitUp+ "\" Util=\"" +utilization1+ "\" />");
 			monitoringProcessedBytesPerUnitUp = 0;
 			CloudSim.bwTotalutil += utilization1;
@@ -273,7 +274,7 @@ public class Link {
 				&& this.highOrder instanceof IntercloudSwitch != true
 				&& this.lowOrder instanceof GatewaySwitch != true
 				&& this.highOrder instanceof GatewaySwitch != true) {
-			LogWriter log = LogWriter.getLogger("./OutputFiles/bandwidthUtil/link_utilization.xml");
+			LogWriter log = LogWriter.getLogger(Constants.outputPath+"\\link_utilization.xml");
 			log.printLine("\t<Link Name=\"" +this.linkname+ "\" Starttime=\"" +String.format("%.6f", logTime-timeUnit) + "\" Endtime=\"" +String.format("%.6f", logTime) +"\" Src=\"" +this.highOrder+ "\" Dst=\"" +this.lowOrder+ "\" KBytes=\"" +monitoringProcessedBytesPerUnitDown+ "\" Util=\"" +utilization2+ "\" />");
 			monitoringProcessedBytesPerUnitDown = 0;
 			CloudSim.bwTotalutil += utilization2;
